6599ae75ebfbf6d2748de90845e188ec987aee18
KOUASSI OTHNIEL <konanothnielr1@gmail.com>
2020-04-30 19:23:32 +0200

    mail

--- mail/src/main/java/com/test/email/demo/My_Contants.java
+++
@@ -1,8 +0,0 @@
-package com.test.email.demo;
-
-public class My_Contants {
-
-/*    public static final String EMAIL = "kouassi-othniel.konan@etudiant.univ-rennes1.fr";
-    public static final String PASSWORD = "0thn13L@@";*/
-    public static final String  FRIEND_EMAIL = "herve-fabrice.kadio@etudiant.univ-rennes1.fr";
-}
